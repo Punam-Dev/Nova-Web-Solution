fc0f92376c84cd2f46d21f00e2de3adb039b62f9
Punam-Dev <punam.pal.dev@gmail.com>
2021-03-06 12:06:38 +0530

    Dtos/DBSqlDto.cs and Dtos/HomeCardDto.cs

---
+++ Dtos/DBSqlDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace NovaWebSolution.Dtos
+{
+    public class DBSqlDto
+    {
+        public string RawSQL { get; set; }
+        public DataTable RawList { get; set; }
+        public int? NoOfRowsAffected { get; set; }
+    }
+}
---
+++ Dtos/HomeCardDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovaWebSolution.Dtos
+{
+    public class HomeCardDto
+    {
+        public int TotalForm { get; set; }
+        public int SaveForm { get; set; }
+        public int SubmitForm { get; set; }
+    }
+}
