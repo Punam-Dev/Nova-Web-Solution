af355c53467d890befaf9a1276e3a9c064de68f3
Punam-Dev <punam.pal.dev@gmail.com>
2021-04-07 00:55:19 +0530

    ChangePasswordDto

---
+++ Dtos/ChangePasswordDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NovaWebSolution.Dtos
+{
+    public class ChangePasswordDto
+    {
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "New password and confirm password must match")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
